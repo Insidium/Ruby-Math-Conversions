62f046dff4888b18125d42641b787537df4b6012
Ryan Challen <ryanchallen@outlook.com>
2020-01-10 12:53:07 +1000

    Added index multiplication logic

--- binary2decimal.rb
+++ binary2decimal.rb
@@ -1,19 +1,36 @@
 def binary_to_decimal(binary)
     base_two = []
-    base_ten = decimal 
+    # base_ten = decimal 
 
-split input after each character, reverse and store in array 
+    # split input after each character, reverse and store in array 
+    binary.to_s.chars.map(&:to_i)
+    # multiply indexes in array: index 6 with 64, 5 with 32, 4 with 16, 3 with 8 etc.
+    
+    case base_two
+    when [6]
+        [6] * 64
+    when [5]
+        [5] * 32
+    when [4]
+        [4] * 16
+    when [3]
+        [3] * 8
+    when [2]
+        [2] * 4
+    when [1]
+        [1] * 2
+    when [0]
+        [0] * 1
+    end  
 
-multiply indexes in array: index 6 with 64, 5 with 32, 4 with 16, 3 with 8 etc.
+    # and store back in array
 
-and store back in array
+# add each returned index result together
 
-add each returned index result together
+# output total 
 
-output total 
-
-    return decimal
+#     return decimal
 
 end
 
-p binary_to_decimal()
+p binary_to_decimal(1234)
